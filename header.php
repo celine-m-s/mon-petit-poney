0d9b4f403f47b90e75ed7f4b6167252f44e70796
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-06-20 18:41:53 +0200

    Mon poney pimpé avec Sass

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Blog photographie</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+
+</head>
+
+<body>
+	<header>
+		<div class="black-mobile">
+			<h1><img src="images/logo-poney.png" alt="Ma vie de Poney"></h1>
+			<img src="images/poney.jpg" width="200px" class="rond">
+			<nav>
+				<ul class="main-nav">
+					<li><a href="#life">Ma vie</a></li>
+					<li><a href="#folks">Mes copains</a></li>
+					<li><a href="#love">J'aime !</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
